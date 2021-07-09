937361b26a26308af36ddcfa3ebc4a5fd174f8f8
Shonak Joshi <shonak.joshi@raspberrypi.com>
2021-07-09 15:56:52 +0100

    Sobel Stage with reduced number of matrices

--- postprocess/sobel_cv_stage.cpp
+++ postprocess/sobel_cv_stage.cpp
@@ -16,11 +16,10 @@ void SobelCVStage::process(CompletedRequest &completed_request)
 	void *mem = app_->Mmap(completed_request.buffers[stream])[0];
 
 	uint8_t *ptr = (uint8_t *)mem;
-	Mat src = Mat(h, stride, CV_8U, ptr);
-	Mat grad;
+	Mat src = Mat(h, w, CV_8U, ptr, stride);
 	int scale = 1;
 	int delta = 0;
-	int ddepth = CV_16U;
+	int ddepth = CV_16S;
 
 	memset(ptr + stride * h, value, num);
 
@@ -34,7 +33,6 @@ void SobelCVStage::process(CompletedRequest &completed_request)
 	GaussianBlur(src, src, Size(3, 3), 0, 0, BORDER_DEFAULT);
 
 	Mat grad_x, grad_y;
-	Mat abs_grad_x, abs_grad_y;
 
 	//Scharr(src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT);
 	Sobel(src, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
@@ -42,11 +40,9 @@ void SobelCVStage::process(CompletedRequest &completed_request)
 	Sobel(src, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
 
 	// converting back to CV_8U
-	convertScaleAbs(grad_x, abs_grad_x);
-	convertScaleAbs(grad_y, abs_grad_y);
+	convertScaleAbs(grad_x, grad_x);
+	convertScaleAbs(grad_y, grad_y);
 
 	//weight the x and y gradients and add their magnitudes
-	addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-
-	memcpy(ptr, grad.ptr(), h * stride);
+	addWeighted(grad_x, 0.5, grad_y, 0.5, 0, src);
 }
